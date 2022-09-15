87ef6f1224c41f2fd22f79d5dc5e592fb64665fc
Kirill Kurentsov <immortalsteel@gmail.com>
2022-09-15 19:27:55 +0300

    Add Smoothly.js
    Add phrase objects, animations, font-size changer, looped animations at start-up

--- script.js
+++ script.js
@@ -1,10 +1,45 @@
+// Add text-image objects for each phrase
 let phrases = [
-  "отправить другу смешную гифку",
-  "посмотреть скидки на авиабилеты",
-  "разобраться, о чём поют рэперы",
-  "Юрий Дудь",
-  "расставить книги на полке по цвету",
-  "читать про зарплаты в Сан-Франциско",
+  {
+    text: "отправить другу смешную гифку",
+    image: "https://code.s3.yandex.net/web-code/procrastinate/1.gif",
+  },
+  {
+    text: "посмотреть скидки на авиабилеты",
+    image: "https://code.s3.yandex.net/web-code/procrastinate/2.png",
+  },
+  {
+    text: "разобраться, о чём поют рэперы",
+    image: "https://code.s3.yandex.net/web-code/procrastinate/3.png",
+  },
+  {
+    text: "Юрий Дудь",
+    image: "https://code.s3.yandex.net/web-code/procrastinate/4.png",
+  },
+  {
+    text: "расставить книги на полке по цвету",
+    image: "https://code.s3.yandex.net/web-code/procrastinate/5.png",
+  },
+  {
+    text: "читать про зарплаты в Сан-Франциско",
+    image: "https://code.s3.yandex.net/web-code/procrastinate/6.png",
+  },
+  {
+    text: "прочитать новости и ужаснуться в комментариях",
+    image: "https://code.s3.yandex.net/web-code/procrastinate/7.png",
+  },
+  {
+    text: "попасть в поток грустных песен и вспомнить все ошибки молодости",
+    image: "https://code.s3.yandex.net/web-code/procrastinate/8.png",
+  },
+  {
+    text: "посмотреть трейлер сериала и заодно первый сезон",
+    image: "https://code.s3.yandex.net/web-code/procrastinate/9.png",
+  },
+  {
+    text: "проверить непрочитанное в Telegram-каналах",
+    image: "https://code.s3.yandex.net/web-code/procrastinate/10.png",
+  },
 ];
 
 function getRandomElement(arr) {
@@ -18,5 +53,21 @@ let phrase = document.querySelector(".phrase");
 let image = document.querySelector(".image");
 
 button.addEventListener("click", function () {
-  phrase.textContent = getRandomElement(phrases);
+  let randomElement = getRandomElement(phrases);
+  // Add cozy animations for text and images
+  smoothly(phrase, "textContent", randomElement.text);
+  smoothly(image, "src", randomElement.image);
+
+  // Add font-size changer for long phrases
+  if (randomElement.text.length > 40) {
+    advice.style.fontSize = "33px";
+  } else {
+    advice.style.fontSize = "42px";
+  }
 });
+
+// Add some looped animations when page loads
+for (let i = 0; i <= 2; i++) {
+  smoothly(phrase, "textContent", phrases[i].text);
+  smoothly(image, "src", phrases[i].image);
+}
